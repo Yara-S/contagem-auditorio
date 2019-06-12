aff8b5ae315e2c22ab1800471aa58a68a74d2ab7
Yara-S <dsouza.yara@Gmail.com>
2019-06-12 11:21:05 -0300

    Javascript script for window interaction

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var contaTela = document.getElementById('N_contagem');
 var alerta = document.getElementById('alerta_contagem');
 var btn_retirar_aluno = document.getElementById('retirar_aluno');
 var zerar = document.getElementById('reset');
+var alert_box = document.getElementById('alert_box');
 var contagemAuditorio = 0;
+
+
+
 
 btn_retirar_aluno.disabled = "true";
 
@@ -24,6 +28,8 @@ botao_clicado.onclick = function(){
 	else if (contagemAuditorio == 50){
 		botao_clicado.disabled = "true";
 		window.alert("Limite atingido. Alunos precisam sair para outros entrarem");
+		alert_box.removeAttribute("hidden");
+
 	}
 
 }
@@ -31,6 +37,7 @@ botao_clicado.onclick = function(){
 btn_retirar_aluno.onclick = function(){
 	if (contagemAuditorio == 50){
 		botao_clicado.removeAttribute("disabled");
+		alert_box.hidden = "true";
 	}
 
 	contagemAuditorio -= 1;
